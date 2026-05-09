42266e6aacee5ea904e7a8cc8041f3f82f9ee0d8
AngusMorton <angus.m.morton@gmail.com>
2026-05-09 20:19:33 +1000

    .

--- packages/vscode/src/__tests__/definition.test.ts
+++ packages/vscode/src/__tests__/definition.test.ts
@@ -1,4 +1,5 @@
 import snap from "mocha-snap";
+import { setTimeout } from "timers/promises";
 import vscode from "vscode";
 
 import {
@@ -23,6 +24,29 @@ describe("definition", () => {
 
 async function definition(src: string) {
   await updateTestDoc(src);
+  const location = await waitForDefinition(src);
+
+  return relativeToTempDir(location.targetUri.fsPath);
+}
+
+async function waitForDefinition(src: string) {
+  const start = Date.now();
+
+  do {
+    const location = await getDefinition();
+
+    if (location) return location;
+    await setTimeout(100);
+    await updateTestDoc(src);
+  } while (Date.now() - start < 5000);
+
+  const { line, character } = getTestEditor().selection.start;
+  throw new Error(
+    `No definition found within 5000ms at ${line}:${character} in ${getTestDoc().uri.fsPath}`,
+  );
+}
+
+async function getDefinition() {
   const [location] = await vscode.commands.executeCommand<
     vscode.LocationLink[]
   >(
@@ -30,5 +54,6 @@ async function definition(src: string) {
     getTestDoc().uri,
     getTestEditor().selection.start,
   );
-  return relativeToTempDir(location.targetUri.fsPath);
+
+  return location;
 }
